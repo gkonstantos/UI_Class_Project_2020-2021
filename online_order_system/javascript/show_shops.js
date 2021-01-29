05b4be469943fa9406395cd27c2056bc31f1e036
Sirodo8plp <te01234@hotmail.com>
2021-01-29 20:53:22 +0200

    Delete all files before updating git repo

--- online_order_system/javascript/show_shops.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll(".shop").forEach((node) => {
-    node.addEventListener("click",(div) => {
-        let target = div.explicitOriginalTarget.attributes[0].value;
-        if(target.indexOf("starbucks") !== -1) location.href = 'order.html' + '#' + 'Starbucks';
-        else if(target.indexOf("mikel") !== -1) location.href = 'order.html' + '#' + 'Mikel';
-        else if(target.indexOf("coffeeisland") !== -1) location.href = 'order.html' + '#' + 'CoffeeIsland';
-    })
-});
