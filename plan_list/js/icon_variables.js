2e1bc95fcb1b61190c401243c6bceac3f7fc5085
Sirodo8plp <te01234@hotmail.com>
2021-01-29 20:55:59 +0200

    Reorganised project.It still needs responsiveness.

---
+++ plan_list/js/icon_variables.js
@@ -0,0 +1,13 @@
+let edit_icon = document.createElement('ion-icon');
+edit_icon.setAttribute('name' , 'pencil-outline');
+edit_icon.addEventListener('click' , expand_Route_Details);
+
+let expand_icon = document.createElement('ion-icon');
+edit_icon.setAttribute('name' , 'arrow-forward-outline');
+edit_icon.addEventListener('click' , expand_Route_Details);
+
+let delete_icon = document.createElement('ion-icon');
+edit_icon.setAttribute('name' , 'close-circle-outline');
+edit_icon.addEventListener('click' , expand_Route_Details);
+
+
